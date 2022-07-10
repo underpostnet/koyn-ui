a53f38bce30e7fbb87838aa3bea4402270ec9186
fcoverdugo <fcoverdugoa@underpost.net>
2022-07-10 15:39:08 -0400

    update

--- src/client.js
+++ src/client.js
@@ -1,149 +1,150 @@
 
 
-const s = _el => document.querySelector(_el);
-const htmls = (_el, _html) => s(_el).innerHTML = _html;
-const append = (_el, _html) => s(_el).insertAdjacentHTML('beforeend', _html);
-const s4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-const range = (start, end) => {
-    return Array.apply(0, Array(end - start + 1))
-        .map((element, index) => index + start);
-};
+(() => {
 
-const fadeIn = (el, display) => {
-    el.style.opacity = 0;
-    el.style.display = display || 'block';
-    const fade = () => {
-        var val = parseFloat(el.style.opacity);
-        if (!((val += .1) > 1)) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-        }
+    const s = _el => document.querySelector(_el);
+    const htmls = (_el, _html) => s(_el).innerHTML = _html;
+    const append = (_el, _html) => s(_el).insertAdjacentHTML('beforeend', _html);
+    const s4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    const range = (start, end) => {
+        return Array.apply(0, Array(end - start + 1))
+            .map((element, index) => index + start);
     };
-    fade();
-};
 
-const borderChar = (px_, color_) => {
-    return `
+    const fadeIn = (el, display) => {
+        el.style.opacity = 0;
+        el.style.display = display || 'block';
+        const fade = () => {
+            var val = parseFloat(el.style.opacity);
+            if (!((val += .1) > 1)) {
+                el.style.opacity = val;
+                requestAnimationFrame(fade);
+            }
+        };
+        fade();
+    };
+
+    const borderChar = (px_, color_) => {
+        return `
 	text-shadow: `+ px_ + `px -` + px_ + `px ` + px_ + `px ` + color_ + `,
 							 -`+ px_ + `px ` + px_ + `px ` + px_ + `px ` + color_ + `,
 							 -`+ px_ + `px -` + px_ + `px ` + px_ + `px ` + color_ + `,
 							 `+ px_ + `px ` + px_ + `px ` + px_ + `px ` + color_ + `;
 	`;
-};
+    };
 
-const renderLang = langs => {
-    if (langs[s('html').lang]) return langs[s('html').lang];
-    return langs['en'];
-};
-// s('html').lang = 'en';
+    const renderLang = langs => {
+        if (langs[s('html').lang]) return langs[s('html').lang];
+        return langs['en'];
+    };
+    // s('html').lang = 'en';
 
-const renderSpinner = (IDS) => {
-    return /*html*/`
+    const renderSpinner = (IDS) => {
+        return /*html*/`
         <div class='in ${IDS}' style='text-align: center; display: none;'>
             <div class='lds-ellipsis'><div></div><div></div><div></div><div></div></div>
         </div>
     `
-};
+    };
 
-const maxIdComponent = 50;
-const errorIcon = /*html*/`<i class='fa fa-exclamation-triangle' aria-hidden='true'></i>`;
-const sucessIcon = /*html*/`<i class='fa fa-check-circle' aria-hidden='true'></i>`;
+    const maxIdComponent = 50;
+    const errorIcon = /*html*/`<i class='fa fa-exclamation-triangle' aria-hidden='true'></i>`;
+    const sucessIcon = /*html*/`<i class='fa fa-check-circle' aria-hidden='true'></i>`;
 
-const CREATE_KEY = {
-    IDS: range(0, maxIdComponent).map(() => 'CREATE_KEY-' + s4()),
-    init: function () {
-        const labelInputs = [8, 9];
-        const inputValueContent = [7, 0];
-        const errorsIdInput = [6, 5];
-        const topLabelInput = '30px';
-        const botLabelInput = '0px';
+    const CREATE_KEY = {
+        IDS: range(0, maxIdComponent).map(() => 'CREATE_KEY-' + s4()),
+        init: function () {
+            const labelInputs = [8, 9];
+            const inputValueContent = [7, 0];
+            const errorsIdInput = [6, 5];
+            const topLabelInput = '30px';
+            const botLabelInput = '0px';
 
-        setTimeout(() => {
+            setTimeout(() => {
 
-            const renderMsgInput = (IDS, MSG, STATUS) => {
-                htmls('.' + this.IDS[IDS], (STATUS ? sucessIcon : errorIcon) + MSG);
-                fadeIn(s('.' + this.IDS[IDS]));
-            };
+                const renderMsgInput = (IDS, MSG, STATUS) => {
+                    htmls('.' + this.IDS[IDS], (STATUS ? sucessIcon : errorIcon) + MSG);
+                    fadeIn(s('.' + this.IDS[IDS]));
+                };
 
-            const checkInput = (i, inputId) => {
-                if (s('.' + this.IDS[inputId]).value == '') {
-                    s('.' + this.IDS[labelInputs[i]]).style.top = topLabelInput;
-                    renderMsgInput(errorsIdInput[i], renderLang({ es: 'Campo vacio', en: 'Empty Field' }));
-                    return false;
-                } else {
+                const checkInput = (i, inputId) => {
+                    if (s('.' + this.IDS[inputId]).value == '') {
+                        s('.' + this.IDS[labelInputs[i]]).style.top = topLabelInput;
+                        renderMsgInput(errorsIdInput[i], renderLang({ es: 'Campo vacio', en: 'Empty Field' }));
+                        return false;
+                    }
                     s('.' + this.IDS[labelInputs[i]]).style.top = botLabelInput;
                     s('.' + this.IDS[errorsIdInput[i]]).style.display = 'none';
                     return true;
-                }
-            };
-
-            const checkAllInput = (setEvent) => inputValueContent.map((inputId, i) => {
-                if (setEvent) {
-                    s('.' + this.IDS[inputId]).onblur = () =>
-                        checkInput(i, inputId);
-                    s('.' + this.IDS[inputId]).oninput = () =>
-                        checkInput(i, inputId);
-                    s('.' + this.IDS[labelInputs[i]]).onclick = () =>
-                        s('.' + this.IDS[inputId]).focus();
-                    s('.' + this.IDS[inputId]).onclick = () =>
-                        s('.' + this.IDS[labelInputs[i]]).style.top = botLabelInput;
-                    s('.' + this.IDS[inputId]).onfocus = () =>
-                        s('.' + this.IDS[labelInputs[i]]).style.top = botLabelInput;
-                    return;
                 };
-                return s('.' + this.IDS[inputId]).oninput();
-            }).filter(x => x == false).length === 0;
 
-            const resetInputs = () => {
-                s('.' + this.IDS[3]).style.display = 'none';
-                fadeIn(s('.' + this.IDS[4]));
-                [12, 5, 6, 11].map(errorId => s('.' + this.IDS[errorId]).style.display = 'none');
-                inputValueContent.map((inputId, i) => {
-                    s('.' + this.IDS[inputId]).value = '';
-                    s('.' + this.IDS[labelInputs[i]]).style.top = topLabelInput;
-                });
-            };
+                const checkAllInput = (setEvent) => inputValueContent.map((inputId, i) => {
+                    if (setEvent) {
+                        s('.' + this.IDS[inputId]).onblur = () =>
+                            checkInput(i, inputId);
+                        s('.' + this.IDS[inputId]).oninput = () =>
+                            checkInput(i, inputId);
+                        s('.' + this.IDS[labelInputs[i]]).onclick = () =>
+                            s('.' + this.IDS[inputId]).focus();
+                        s('.' + this.IDS[inputId]).onclick = () =>
+                            s('.' + this.IDS[labelInputs[i]]).style.top = botLabelInput;
+                        s('.' + this.IDS[inputId]).onfocus = () =>
+                            s('.' + this.IDS[labelInputs[i]]).style.top = botLabelInput;
+                        return;
+                    };
+                    return s('.' + this.IDS[inputId]).oninput();
+                }).filter(x => x == false).length === 0;
 
-            checkAllInput(true);
-            s('.' + this.IDS[10]).onclick = e => setTimeout(() => resetInputs());
-            s('.' + this.IDS[1]).onclick = e => {
-                e.preventDefault();
-                console.log('onclick', s('.' + this.IDS[0]).value);
-                if (!checkAllInput()) return;
-                s('.' + this.IDS[2]).style.display = 'none';
-                s('.' + this.IDS[4]).style.display = 'none';
-                fadeIn(s('.' + this.IDS[3]));
-                const errorMsgService =
-                    renderLang({ es: 'Error en el Servicio', en: 'Service Error' });
-                fetch('/api/keys/create-key', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        passphrase: s('.' + this.IDS[0]).value,
-                        name: s('.' + this.IDS[7]).value
-                    }),
-                })
-                    .then((res) => res.json())
-                    .then((res) => {
-                        resetInputs();
-                        if (res.status == 'error') {
-                            console.log('POST ERROR - /create-key', res.data);
-                            return renderMsgInput(11, errorMsgService);
-                        }
-                        console.log('POST SUCCESS - /create-key', res.data);
-                        htmls('.' + this.IDS[2], res.data.privateKey);
-                        fadeIn(s('.' + this.IDS[2]));
-                        return renderMsgInput(12, renderLang({ es: 'Las llaves han sido creadas', en: 'The keys have been created' }), true);
-                    }).catch(error => {
-                        console.log('POST ERROR - /create-key', error);
-                        return renderMsgInput(11, errorMsgService);
+                const resetInputs = () => {
+                    s('.' + this.IDS[3]).style.display = 'none';
+                    fadeIn(s('.' + this.IDS[4]));
+                    [12, 5, 6, 11].map(errorId => s('.' + this.IDS[errorId]).style.display = 'none');
+                    inputValueContent.map((inputId, i) => {
+                        s('.' + this.IDS[inputId]).value = '';
+                        s('.' + this.IDS[labelInputs[i]]).style.top = topLabelInput;
                     });
+                };
 
-            };
-        });
-        return /*html*/`
+                checkAllInput(true);
+                s('.' + this.IDS[10]).onclick = e => setTimeout(() => resetInputs());
+                s('.' + this.IDS[1]).onclick = e => {
+                    e.preventDefault();
+                    console.log('onclick', s('.' + this.IDS[0]).value);
+                    if (!checkAllInput()) return;
+                    s('.' + this.IDS[2]).style.display = 'none';
+                    s('.' + this.IDS[4]).style.display = 'none';
+                    fadeIn(s('.' + this.IDS[3]));
+                    const errorMsgService =
+                        renderLang({ es: 'Error en el Servicio', en: 'Service Error' });
+                    fetch('/api/keys/create-key', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            passphrase: s('.' + this.IDS[0]).value,
+                            name: s('.' + this.IDS[7]).value
+                        }),
+                    })
+                        .then((res) => res.json())
+                        .then((res) => {
+                            resetInputs();
+                            if (res.status == 'error') {
+                                console.log('POST ERROR - /create-key', res.data);
+                                return renderMsgInput(11, errorMsgService);
+                            }
+                            console.log('POST SUCCESS - /create-key', res.data);
+                            htmls('.' + this.IDS[2], res.data.privateKey);
+                            fadeIn(s('.' + this.IDS[2]));
+                            return renderMsgInput(12, renderLang({ es: 'Las llaves han sido creadas', en: 'The keys have been created' }), true);
+                        }).catch(error => {
+                            console.log('POST ERROR - /create-key', error);
+                            return renderMsgInput(11, errorMsgService);
+                        });
+
+                };
+            });
+            return /*html*/`
             <div class='in container'>
                 <form class='in ${this.IDS[4]}'>
 
@@ -174,27 +175,27 @@ const CREATE_KEY = {
                 ${renderSpinner(this.IDS[3])}
             </div>
         `
-    }
-};
+        }
+    };
 
 
-const TABLE_KEYS = {
-    IDS: range(0, maxIdComponent).map(() => 'TABLE_KEYS-' + s4()),
-    renderTable: function () {
+    const TABLE_KEYS = {
+        IDS: range(0, maxIdComponent).map(() => 'TABLE_KEYS-' + s4()),
+        renderTable: function () {
 
-    },
-    init: function () {
+        },
+        init: function () {
 
-        return /*html*/`
+            return /*html*/`
             <div class=' in container ${this.IDS[0]}'>
                     table keys
             </div>
         `;
-    }
-};
+        }
+    };
 
 
-append('body', /*html*/`
+    append('body', /*html*/`
         <div class='in container main-title' style='${borderChar(1, 'yellow')}'>
                KOYN UI v1.0.0
         </div>
@@ -203,4 +204,4 @@ append('body', /*html*/`
 
 `);
 
-
+})();
