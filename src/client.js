7dc5d873a44ad6701f96fb6e621f205a5e7a4ea4
fcoverdugo <fcoverdugoa@underpost.net>
2022-07-10 00:24:04 -0400

    update

--- src/client.js
+++ src/client.js
@@ -9,18 +9,37 @@ const range = (start, end) => {
         .map((element, index) => index + start);
 };
 
+const fadeIn = (el, display) => {
+    el.style.opacity = 0;
+    el.style.display = display || 'block';
+    const fade =  () =>  {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    };
+    fade();
+};
 
+const spinner = /*html*/`
+             <div class='inl'>
+                  <div class='lds-ellipsis'><div></div><div></div><div></div><div></div></div>
+             </div>
+`;
 
 const CREATE_KEY = {
     init: () => {
 
-        const IDS = range(0, 3).map(() => 'CREATE_KEY-' + s4());
+        const IDS = range(0, 4).map(() => 'CREATE_KEY-' + s4());
 
         setTimeout(() => {
             s('.' + IDS[1]).onclick = e => {
                 e.preventDefault();
                 console.log('onclick', s('.' + IDS[0]).value);
-
+                s('.' + IDS[2]).style.display = 'none';
+                s('.' + IDS[4]).style.display = 'none';
+                fadeIn(s('.' + IDS[3]));
                 fetch('/create-key', {
                     method: 'POST',
                     headers: {
@@ -31,8 +50,10 @@ const CREATE_KEY = {
                     .then((res) => res.json())
                     .then((res) => {
                         console.log('POST - /create-key', res);
-                        htmls('.' + IDS[3], res.privateKey);
-                        s('.' + IDS[2]).style.display = 'block';
+                        htmls('.' + IDS[2], res.privateKey);
+                        s('.' + IDS[3]).style.display = 'none';
+                        fadeIn(s('.' + IDS[2]));
+                        fadeIn(s('.' + IDS[4]));
                     });
 
             };
@@ -42,14 +63,14 @@ const CREATE_KEY = {
                 KOYN UI v1.0.0
             </div>
             <div class='in container'>
-                <form class='in'>
+                <form class='in ${IDS[4]}'>
                   ${{ es: 'Contraseña llave publica', en: 'Public Key password' }[s('html').lang]}
                   <input class='${IDS[0]}' type='password' autocomplete='new-password' placeholder=' ...'>
                   <button class='${IDS[1]}'><i class='fa fa-key' aria-hidden='true'></i>${{ es: 'Crear llaves', en: 'Create keys' }[s('html').lang]}</button>
                 </form>
-                <div class='in ${IDS[2]}' style='font-size: 12px; display: none; overflow: auto'>
-                    <pre class='${IDS[3]}'></pre>
-                </div>
+                <pre class='in ${IDS[2]}' style='display: none;'></pre>
+                
+                <div class='in ${IDS[3]}' style='text-align: center; display: none;'>${spinner}</div>
             </div>
         `
     }
